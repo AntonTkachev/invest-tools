ff4c000cd628a69c77efe6776f63ee66e8d78a47
Аnton <dgeneros@mail.ru>
2018-04-21 13:24:29 +0300

    draft

---
+++ backend/src/main/java/i/t/repository/MainFormRepository.java
@@ -0,0 +1,7 @@
+package i.t.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import i.t.entity.MainForm;
+
+public interface MainFormRepository extends CrudRepository<MainForm, Long> {
+}
