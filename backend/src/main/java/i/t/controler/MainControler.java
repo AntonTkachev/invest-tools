f683fb9b99519a1bec0fdef862298b4af22f10cf
AntonTkachev <gmred@mail.ru>
2018-05-08 11:49:18 +0300

    Update MainControler.java

--- backend/src/main/java/i/t/controler/MainControler.java
+++ backend/src/main/java/i/t/controler/MainControler.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+//TODO @RepositoryrestController
+//TODO @RequestMapping
 @Controller
 public class MainControler {
     @Autowired
     private MainFormRepository mainFormRepository;
 
     @RequestMapping(value = "api/mainForms/{id}", method = RequestMethod.GET)
+    //TODO @ResponceBody
     public MainForm get(@PathVariable("id") Long id) {
         MainForm findMainForm = mainFormRepository.findOne(id);
         return findMainForm;
